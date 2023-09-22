f355a90e75fc325e9d564d31723bf251649c4a70
turelyk-arsen <114808096+turelyk-arsen@users.noreply.github.com>
2023-09-22 22:30:43 +0200

    add doctrine

--- src/Users/Infrastructure/Repository/UserRepository.php
+++ src/Users/Infrastructure/Repository/UserRepository.php
@@ -3,11 +3,11 @@ declare(strict_types=1);
 
 namespace App\Users\Infrastructure\Repository;
 
-use App\Users\Domain\Repository\UserRepositoryInterface;
+// use App\Users\Domain\Repository\UserRepositoryInterface;
 
-class UserRepository implements UserRepositoryInterface
-{
+// class UserRepository implements UserRepositoryInterface
+// {
     
-}
+// }
 
 ?>
